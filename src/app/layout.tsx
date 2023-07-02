f682e2f800ba97f96dbd879d563ed1662b125dbe
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-07-02 15:16:44 +0530

    removed unwanted npm s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Providers>
-        {children}
+        <Providers>
+          {children}
         </Providers>
-        </body>
+      </body>
     </html>
   )
 }
